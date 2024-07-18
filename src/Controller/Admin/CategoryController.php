29ec47a49e0d61170c8aa7f01cfa325eae027cda
OuraaD <ouradiarra2@gmail.com>
2024-07-18 14:06:26 +0200

    update

--- src/Controller/Admin/CategoryController.php
+++ src/Controller/Admin/CategoryController.php
@@ -3,53 +3,79 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Category;
+use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
 use DateTimeImmutable;
+use phpDocumentor\Reflection\Types\This;
 use Symfony\Component\Routing\Requirement\Requirement;
+use Symfony\Component\HttpFoundation\Request;
 
 #[Route('/admin/category', 'admin_category_')]
 class CategoryController extends AbstractController
 {
-    #[Route('/', name: 'index')]
-    public function index()
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
     {
-        return $this->render('admin/category/index.html.twig');
+        $this->em = $em;
+    }
+
+    #[Route('/', name: 'index')]
+    public function index(CategoryRepository $repository)
+    {
+        $categories = $repository->findAll();
+
+        return $this->render('admin/category/index.html.twig', [
+            'categories' => $categories
+        ]);
     }
 
     #[Route('/create', name: 'create')]
-    public function create(EntityManagerInterface $em)
+    public function create()
     {
         $category = new Category();
         $category->setNom('bonbons')
             ->setDescription('Meilleurs bonbons du monde')
             ->setCreateAt(new \DateTimeImmutable());
 
-        $em->persist($category);
-        $em->flush();
+        $this->em->persist($category);
+        $this->em->flush();
 
         return $this->render('admin/category/create.html.twig');
     }
 
     #[Route('/update/{id}', name: 'update', requirements: ['id' => Requirement::DIGITS])]
-    public function update(Category $category, EntityManagerInterface $em)
+    public function update(Category $category, Request $request)
     {
-        $category->setNom('Bonbons')
-        ->setUpdateAt(new \DateTimeImmutable());
+        $form = $this->createForm(CategoryType::class, $category);
 
-        $em->flush();
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->flush();
+            return $this->redirectToRoute('admin_category_index');
+        }
+
+        return $this->render('admin/category/update.html.twig', [
+            'formCategory' => $form
+        ]);
+        // $category->setNom('Bonbons')
+        // ->setUpdateAt(new \DateTimeImmutable());
+
+        // $em->flush();
 
         return $this->render('admin/category/update.html.twig');
     }
 
     #[Route('/delete/{id}', name: 'delete', requirements: ['id' => Requirement::DIGITS])]
-    public function delete(EntityManagerInterface $em, Category $category)
+    public function delete(Category $category)
     {
-        $em->remove($category);
+        $this->em->remove($category);
 
-        $em->flush();
+        $this->em->flush();
 
         return $this->render('admin/category/delete.html.twig');
     }
--- src/Controller/Front/HomeController.php
+++ src/Controller/Front/HomeController.php
@@ -3,13 +3,16 @@
 namespace App\Controller\Front;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'front_home_index')]
-    public function index()
+    public function index():Response
     {
-        return $this->render('front/home/index.html.twig');
+        return $this->render('front/home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
     }
 }
